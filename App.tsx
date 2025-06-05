000134fa5b88ed7b0981ea511b00f774b8935a90
KaueFulgencio <Kauemfulgencio@outlook.com>
2025-06-05 01:55:47 -0300

    biblioteca do gesture handler

--- App.tsx
+++ App.tsx
@@ -3,14 +3,16 @@ import AppNavigator from './src/navigation/AppNavigator';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './src/styles/theme';
 import { AccessibilityProvider } from './src/context/AccessibilityContext';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
-  
   return (
-    <ThemeProvider theme={theme}>
-      <AccessibilityProvider>
-        <AppNavigator />
-      </AccessibilityProvider>
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider theme={theme}>
+        <AccessibilityProvider>
+          <AppNavigator />
+        </AccessibilityProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
